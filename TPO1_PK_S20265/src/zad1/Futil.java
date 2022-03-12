1c81ac7da7b93e94cbaebb598494d7c16af469cf
Karol Peła <11260208+irseq@users.noreply.github.com>
2022-03-12 23:38:42 +0100

    Hopefully finished the task

--- TPO1_PK_S20265/src/zad1/Futil.java
+++ TPO1_PK_S20265/src/zad1/Futil.java
@@ -1,12 +1,18 @@
 package zad1;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.channels.*;
+import java.nio.charset.Charset;
 
 public class Futil {
 	public static void processDir(String dirName, String resultFileName) {
@@ -14,7 +20,26 @@ public class Futil {
 		try {
 			Files.walkFileTree(dirPath, new SimpleFileVisitor<Path>() {
 				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-					
+					try (FileInputStream in = new FileInputStream(file.toString());
+							FileOutputStream out = new FileOutputStream("TPO1res.txt", true)) {
+						FileChannel fcin = in.getChannel();
+						FileChannel fcout = out.getChannel();
+						int size = (int) fcin.size();
+						ByteBuffer buf = ByteBuffer.allocate(size);
+
+						fcin.read(buf);
+						buf.flip();
+
+						Charset inCharset = Charset.forName("Cp1250");
+						CharBuffer cbuf = inCharset.decode(buf);
+						Charset outCharset = Charset.forName("UTF-8");
+
+						buf = outCharset.encode(cbuf);
+						fcout.write(buf);
+
+						fcin.close();
+						fcout.close();
+					}
 					return FileVisitResult.CONTINUE;
 				}
 
