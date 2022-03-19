1eb34727233723b10df2c85d9a73c510c5e112d2
Karol Peła <11260208+irseq@users.noreply.github.com>
2022-03-19 20:35:50 +0100

    Use own panel for displaying weather

---
+++ TPO2_PK_S20265/src/zad1/WeatherPanel.java
@@ -0,0 +1,53 @@
+package zad1;
+
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+import zad1.WeatherInfo.WeatherInfo;
+
+final class WeatherPanel extends JPanel {
+	JLabel cityLabel;
+	JLabel conditionsLabel;
+	JLabel tempLabel;
+	JLabel pressureLabel;
+	JLabel humidityLabel;
+	JLabel windLabel;
+
+	public WeatherPanel() {
+		super();
+		this.cityLabel = new JLabel();
+		this.conditionsLabel = new JLabel();
+		this.tempLabel = new JLabel();
+		this.pressureLabel = new JLabel();
+		this.humidityLabel = new JLabel();
+		this.windLabel = new JLabel();
+		add(cityLabel);
+		add(conditionsLabel);
+		add(tempLabel);
+		add(pressureLabel);
+		add(humidityLabel);
+		add(windLabel);
+		//setLayout(new GridLayout(0, 1));
+		setBorder(BorderFactory.createTitledBorder("Weather"));
+	}
+
+	public void updateData(WeatherInfo wi) {
+		//TODO add icons! 🙂
+		cityLabel.setText("City: " 
+			+ wi.getName());
+		conditionsLabel.setText("Conditions: " 
+			+ wi.getWeather().get(0).main);
+		tempLabel.setText("Temperature: "
+			+ wi.getMain().getTemp() + " °C");
+		pressureLabel.setText("Pressure: "
+			+ wi.getMain().getPressure() + " hPa");
+		humidityLabel.setText("Humidity: "
+			+ wi.getMain().getHumidity() + "%");
+		windLabel.setText("Wind: " 
+			+ wi.getWind().getSpeed() + " km/h");
+	}
+}
