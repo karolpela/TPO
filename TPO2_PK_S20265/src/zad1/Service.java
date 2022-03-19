eb0dad012d970f07e771b9fc598c39d5e4a18c97
Karol Peła <11260208+irseq@users.noreply.github.com>
2022-03-19 17:42:03 +0100

    Use stream processing for locale operations

--- TPO2_PK_S20265/src/zad1/Service.java
+++ TPO2_PK_S20265/src/zad1/Service.java
@@ -11,54 +11,46 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Currency;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
 public class Service {
     private static final String API_KEY = "9e3b822f0820bf7d604c31b366029096";
-    public static Map<String, String> countryMap;
-
-    private String country;
-    private String countryCode;
+    
     private Locale locale;
+    private Currency currency;
 
     public Service(String country) {
-        if (Service.countryMap == null)
-            populateMap();
-        this.country = country;
-        Map<String,String> temp = Service.countryMap;
-        this.locale = new Locale(Service.countryMap.get(country));
-        this.countryCode = locale.getCountry();
+        this.locale = Arrays.stream(Locale.getAvailableLocales())
+            .filter(l -> l.getDisplayCountry().equals(country))
+            .findFirst()
+            .get();
+        System.out.println(locale);
+        currency = Currency.getInstance(locale);
+        System.out.println(currency.getCurrencyCode());
     }
 
     public String getWeather(String city) {
-        String s = "";
+        String weatherJson = "";
         try {
             URL weatherCall = new URL(
                 "https://api.openweathermap.org/data/2.5/weather?q="
                 + city + "&units=metric" + "&appid=" + API_KEY
             );
-            System.out.println(weatherCall);
             BufferedReader in = new BufferedReader(new InputStreamReader(weatherCall.openStream(), "UTF-8"));
             String line;
             while((line = in.readLine()) != null) 
-                s += line;
+                weatherJson += line;
         } catch (MalformedURLException e) {
             System.out.println("[!] Malformed URL");
         } catch (IOException e) {
             System.out.println("[!] Error opening stream");
         }
-        System.out.println(s);
-        return s;    
-    }
-
-    private static void populateMap() {
-        countryMap = new HashMap<>();
-        // https://stackoverflow.com/a/14155186/17862415
-        for (String country : Locale.getISOCountries()) {
-            Locale l = new Locale("", country);
-            countryMap.put(l.getDisplayCountry(), country);
-        }
+        return weatherJson;    
     }
 }
