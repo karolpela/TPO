ac1ef50782fa0a6818ddefc832c43835267b84f7
Karol Peła <11260208+irseq@users.noreply.github.com>
2022-04-30 23:25:35 +0200

    Lay groundwork for handling topics and messages

--- TPO4_PK_S20265/src/zad1/Server.java
+++ TPO4_PK_S20265/src/zad1/Server.java
@@ -12,13 +12,21 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Queue;
 import java.util.Set;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Server {
 
     public static final String HOST = "localhost";
     public static final int PORT = 9000;
+
+    private static HashMap<String, List<SocketChannel>> channelsByTopic = new HashMap<>();
+    private static HashMap<SocketChannel, Queue<String>> messagesByChannel = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         new Server();
@@ -51,11 +59,10 @@ public class Server {
                     // get the key and remove it from further iterations
                     SelectionKey key = iterator.next();
                     iterator.remove();
-
                     // if there's a new client connection
                     if (key.isAcceptable()) {
                         System.out.println(this + " got a new connection");
-
+                        System.out.println(key.channel());
                         // create a channel to communicate with client
                         // "accept() is non-blocking since client's already waiting"
                         var socketChannel = serverSocketChannel.accept();
@@ -63,6 +70,10 @@ public class Server {
                         // configure and register the channel
                         socketChannel.configureBlocking(false);
                         socketChannel.register(selector, OP_READ | OP_WRITE);
+
+                        // create a queue for this channel
+                        messagesByChannel.put((SocketChannel) key.channel(),
+                                new LinkedBlockingQueue<>());
 
                         continue;
                     }
@@ -76,11 +87,12 @@ public class Server {
 
                     // if there's a ready-to-write channel
                     if (key.isWritable()) {
-                        // var socketChannel = (SocketChannel) key.channel();
-
-                        // TODO handle response to client
-
-                        // broadcast here probably
+                        var socketChannel = (SocketChannel) key.channel();
+                        var queue = messagesByChannel.get(socketChannel);
+                        String message = queue.poll();
+                        if (message != null) {
+                            writeToChannel(socketChannel, message);
+                        }
                         continue;
                     }
                 }
@@ -152,11 +164,50 @@ public class Server {
                     socketChannel.close();
                     socketChannel.socket().close();
                 }
+                case "ADD_TOPIC" -> {
+                    String topic = arguments;
+                    channelsByTopic.put(topic, new ArrayList<>());
+                    System.out.println(this + "Added new topic \"" + topic + "\"");
+
+                    var allChannels = messagesByChannel.keySet();
+                    for (SocketChannel sc : allChannels) {
+                        messagesByChannel.get(sc).add("ADD_TOPIC;" + topic);
+                    }
+                }
+                case "REMOVE_TOPIC" -> {
+                    String topic = arguments;
+                    channelsByTopic.remove("key");
+                    System.out.println(this + "Removed  topic \"" + topic + "\"");
+
+                    var allChannels = messagesByChannel.keySet();
+                    for (SocketChannel sc : allChannels) {
+                        messagesByChannel.get(sc).add("REMOVE_TOPIC;" + topic);
+                    }
+                }
+                case "MESSAGE" -> {
+                    var argArray = arguments.split("`");
+                    String topic = argArray[0];
+                    String message = argArray[1];
+                    var subscribers = channelsByTopic.get(topic);
+                    for (SocketChannel sc : subscribers) {
+                        messagesByChannel.get(sc).add(message);
+                    }
+                    System.out.println(this + "Added message \"" + message + "\"" +
+                            "to topic \"" + topic + "\"");
+                }
                 case "SUBSCRIBE" -> {
-                    //
+                    String topic = arguments;
+                    var channels = channelsByTopic.get("topic");
+                    channels.add(socketChannel);
+                    writeToChannel(socketChannel,
+                            "Successfully subscribed to \"" + topic + "\"");
                 }
                 case "UNSUBSRIBE" -> {
-                    //
+                    String topic = arguments;
+                    var channels = channelsByTopic.get("topic");
+                    channels.remove(socketChannel);
+                    writeToChannel(socketChannel,
+                            "Successfully unsubscribed from \"" + topic + "\"");
                 }
                 default -> {
                     writeToChannel(socketChannel, command);
