3b3d5dc9897d5088fbc8a4365983da00557401ad
Karol Peła <11260208+irseq@users.noreply.github.com>
2022-05-19 22:56:52 +0200

    Create basic project specific form servlet

--- TPO5_PK_S20265/src/main/java/zad1/FormServlet.java
+++ TPO5_PK_S20265/src/main/java/zad1/FormServlet.java
@@ -3,7 +3,9 @@ package zad1;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.UnsupportedCharsetException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,49 +16,71 @@ public class FormServlet extends HttpServlet {
 
     private static final String CHARSET = "UTF-8";
 
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException, UnsupportedCharsetException {
-        req.setCharacterEncoding(CHARSET);
+    protected void service(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        try {
+            req.setCharacterEncoding(CHARSET);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
         resp.setContentType("text/html");
 
-        PrintWriter out = resp.getWriter();
+        try (PrintWriter out = resp.getWriter()) {
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Car database interface</title>");
+            out.println("</head>");
 
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Request Parameters Example</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h3>Request Parameters Example</h3>");
-        out.println("Parameters in this request:<br>");
+            out.println("<body>");
 
-        String rodzaj = req.getParameter("rodzaj");
-
-        if (rodzaj != null) {
+            out.println("<center><h1> Car database interface </h1></center>");
+            out.print("<form method = \"POST\""
+                    + "form action=\"http://localhost:8080/TPO5/ResponseServlet\"");
+            out.print("RequestParamExample\" ");
+            out.println("method=POST>");
             out.println("Rodzaj:");
-            out.println(" = " + rodzaj);
-        }
+            out.println("<input type=text size=20 name=rodzaj>");
+            out.println("<br>");
+            out.println("<input type=submit>");
+            out.println("</form>");
 
-        out.println("<P>");
-        out.print("<form action=\"");
-        out.print("RequestParamExample\" ");
-        out.println("method=POST>");
-        out.println("First Name:");
-        out.println("<input type=text size=20 name=firstname>");
-        out.println("<br>");
-        out.println("Last Name:");
-        out.println("<input type=text size=20 name=lastname>");
-        out.println("<br>");
-        out.println("<input type=submit>");
-        out.println("</form>");
+            String rodzaj = req.getParameter("rodzaj");
+
+            if (rodzaj == null) {
+                printEndTag(out);
+                return;
+            }
+
+            printEndTag(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void printEndTag(PrintWriter out) {
         out.println("</body>");
         out.println("</html>");
+    }
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException, ServletException {
+        try {
+            service(req, resp);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-
+            throws IOException, ServletException {
+        try {
+            service(req, resp);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
